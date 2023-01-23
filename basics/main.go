980ee979a998d00a45d0a82a4237b99cd9dac737
Mario <marioskrlec222@gmail.com>
2023-01-23 16:29:37 +0100

    Added basics

---
+++ basics/main.go
@@ -0,0 +1,15 @@
+/**
+1. Explain main package
+2. Explain functions
+3. Explain pointers
+4. Explain structs
+5. Explain marshaling ("{"name":"","lastName":"","email":""}")
+6. Explain interfaces
+7. Explain slices
+8. Explain maps
+*/
+
+package main
+
+func main() {
+}
